ca0f767cc05bb08f9fdc0025ee85467db45b449e
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2020-08-12 04:51:04 -0700

    Update r.py

--- r.py
+++ r.py
@@ -1,4 +1,4 @@
 from PIL import Image    
-import pytesseract as tess
-
-print (tess.image_to_string(Image.open('bangla.png'), lang='ben'))
+import pytesseract 
+pytesseract.pytesseract.tesseract_cmd = ‘/app/.apt/usr/bin/tesseract’
+print (pytesseract.image_to_string(Image.open('bangla.png'), lang='ben'))
