3581ed4e1e479b442f69db0c4555c9b10022cd70
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2020-08-12 03:51:25 -0700

    Update r.py

--- r.py
+++ r.py
@@ -1,4 +1,4 @@
 from PIL import Image    
 import pytesseract as tess
 
-print tess.image_to_string(Image.open('bangla.jpg'), lang='ben')
+print tess.image_to_string(Image.open('bangla.png'), lang='ben')
