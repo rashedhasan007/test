8702d1e559c3220301c8793b97d679f0ba2149d6
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2020-08-22 09:31:03 -0700

    Update r.py

--- r.py
+++ r.py
@@ -1,11 +1,25 @@
-import pytesseract
-from PIL import Image, ImageEnhance, ImageFilter
-import os
 
-im = Image.open("j.jpg") # the second one
-im = im.filter(ImageFilter.MedianFilter())
-enhancer = ImageEnhance.Contrast(im)
-im = enhancer.enhance(2)
-im = im.convert('1')
-text = pytesseract.image_to_string(Image.open("j.jpg"),lang="ben")
-print(text.encode('utf-8'))
+from flask import Flask, request #import main Flask class and request object
+import io
+import requests
+from io import BytesIO
+import pytesseract
+from PIL import Image
+app = Flask(__name__) #create the Flask app
+
+@app.route('/')
+def main():
+    return '''<h1>use query-example?=language=passurl</h>'''
+@app.route('/query-example')
+def query_example():
+    language = request.args.get('language') #if key doesn't exist, returns None
+    response = requests.get(language)
+    # print( type(response) ) # <class 'requests.models.Response'>
+    #img = Image.open(io.BytesIO(response.content))
+    img = Image.open(BytesIO(response.content))
+    text = pytesseract.image_to_string(img,lang="ben")
+
+    return '''<h1>The language value is: {}</h1>'''.format(text)
+
+if __name__=="__main__":
+    app.run()
