cd50e20a55332df16215ba49baa35a10fbd44772
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2020-08-22 08:44:19 -0700

    Update r.py

--- r.py
+++ r.py
@@ -2,10 +2,10 @@ import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
-im = Image.open("test.jpg") # the second one
+im = Image.open("j.jpg") # the second one
 im = im.filter(ImageFilter.MedianFilter())
 enhancer = ImageEnhance.Contrast(im)
 im = enhancer.enhance(2)
 im = im.convert('1')
-text = pytesseract.image_to_string(Image.open("test.jpg"),lang="ben")
+text = pytesseract.image_to_string(Image.open("j.jpg"),lang="ben")
 print(text)
