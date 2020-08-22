4a9545d33706a57f53086a3f0da3afa32945a7f8
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2020-08-22 15:24:03 -0700

    Update r.py

--- r.py
+++ r.py
@@ -17,6 +17,8 @@ def query_example():
     # print( type(response) ) # <class 'requests.models.Response'>
     #img = Image.open(io.BytesIO(response.content))
     img = Image.open(BytesIO(response.content))
+    # Convert to gray
+    img=img.convert('L')
     text = pytesseract.image_to_string(img,lang="ben")
 
     return '''<h1>The language value is: {}</h1>'''.format(text)
