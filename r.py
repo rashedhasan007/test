efb39b4b8186cb7f7390847fc9ea8be7edef033c
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2020-09-13 06:46:54 -0700

    Update r.py

--- r.py
+++ r.py
@@ -1,27 +1,6 @@
+import pyspx.shake256_128f
 
-from flask import Flask, request #import main Flask class and request object
-import io
-import requests
-from io import BytesIO
-import pytesseract
-from PIL import Image
-app = Flask(__name__) #create the Flask app
-
-@app.route('/')
-def main():
-    return '''<h1>use query-example?=language=passurl</h>'''
-@app.route('/query-example')
-def query_example():
-    language = request.args.get('language') #if key doesn't exist, returns None
-    response = requests.get(language)
-    # print( type(response) ) # <class 'requests.models.Response'>
-    #img = Image.open(io.BytesIO(response.content))
-    img = Image.open(BytesIO(response.content))
-    # Convert to gray
-    #img=img.convert('L')
-    text = pytesseract.image_to_string(img,lang="ben")
-
-    return '''<h1>The language value is: {}</h1>'''.format(text)
-
-if __name__=="__main__":
-    app.run()
+public_key, secret_key = pyspx.shake256_128f.generate_keypair(seed)
+signature = pyspx.shake256_128f.sign(message, secret_key)
+print(signature)
+print(publickey)
