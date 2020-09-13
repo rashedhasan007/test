f4762e2694aad5314edd3e27871552c84fb6c943
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2020-09-13 07:07:12 -0700

    Update r.py

--- r.py
+++ r.py
@@ -1,6 +1,7 @@
 import pyspx.shake256_128f
 import pyspx.shake256_128f as sphincs
 import os, binascii
+message = b'Message for SPHINCS+ shake256_128f signing'
 seed = os.urandom(sphincs.crypto_sign_SEEDBYTES)
 public_key, secret_key = pyspx.shake256_128f.generate_keypair(seed)
 signature = pyspx.shake256_128f.sign(message, secret_key)
